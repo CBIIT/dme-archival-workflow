049184e067cea3b2022066aa15c1cda67013cb8a
dinhys <dinhys@NCI-02294132-L.nci.nih.gov>
2023-10-30 11:54:37 -0400

    DMESUPPORT-44: Setup archival workflow for DCTD-PCL

--- src/main/java/gov/nih/nci/hpc/dmesync/workflow/custom/impl/DctdPclPathMetadataProcessorImpl.java
+++ src/main/java/gov/nih/nci/hpc/dmesync/workflow/custom/impl/DctdPclPathMetadataProcessorImpl.java
@@ -272,7 +272,7 @@ public class DctdPclPathMetadataProcessorImpl extends AbstractPathMetadataProces
 		String piCollectionName = null;
 		piCollectionName = getAttrValueWithKey(getProjectCollectionName(object), "data_owner");
 		logger.info("PI Collection Name: {}", piCollectionName);
-		if(Character.isWhitespace(piCollectionName.charAt(piCollectionName.length()-1)))
+		if(StringUtils.isEmpty(piCollectionName.substring(piCollectionName.length()-1)))
 			piCollectionName = piCollectionName.substring(0, piCollectionName.length()-1);
 		return piCollectionName;
 	}
@@ -280,7 +280,7 @@ public class DctdPclPathMetadataProcessorImpl extends AbstractPathMetadataProces
 	private String getPOCCollectionName(StatusInfo object) throws DmeSyncMappingException {
 		String pocCollectionName = null;
 		pocCollectionName = getCollectionNameFromParent(object, "DCTD_PCL_Tara_Hiltke");
-		if(Character.isWhitespace(pocCollectionName.charAt(pocCollectionName.length()-1)))
+		if(StringUtils.isEmpty(pocCollectionName.substring(pocCollectionName.length()-1)))
 			pocCollectionName = pocCollectionName.substring(0, pocCollectionName.length()-1);
 		logger.info("PI Collection Name: {}", pocCollectionName);
 		return pocCollectionName;
@@ -289,7 +289,7 @@ public class DctdPclPathMetadataProcessorImpl extends AbstractPathMetadataProces
 	private String getResearcher(StatusInfo object) throws DmeSyncMappingException {
 		String researcher = null;
 		researcher = getAttrValueWithKey(getProjectCollectionName(object), "researcher");
-		if(Character.isWhitespace(researcher.charAt(researcher.length()-1)))
+		if(StringUtils.isEmpty(researcher.substring(researcher.length()-1)))
 			researcher = researcher.substring(0, researcher.length()-1);
 		logger.info("Researcher Name: {}", researcher);
 		return researcher;
