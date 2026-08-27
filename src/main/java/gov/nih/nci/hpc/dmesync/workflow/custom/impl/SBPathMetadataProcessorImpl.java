46c3da24ef34eb7814961c8862de4ea4148fd286
mkoneru <manoja.koneru@nih.gov>
2026-08-27 14:21:24 -0400

    DMESUPPORT-226 code review comments

--- src/main/java/gov/nih/nci/hpc/dmesync/workflow/custom/impl/SBPathMetadataProcessorImpl.java
+++ src/main/java/gov/nih/nci/hpc/dmesync/workflow/custom/impl/SBPathMetadataProcessorImpl.java
@@ -148,8 +148,9 @@ public class SBPathMetadataProcessorImpl extends AbstractPathMetadataProcessor
 		        	ExcelUtil.convertTextToExcel(new File(sampleSheetPath.toString()), new File(excelFilePath.toString()),"\t");
 		        }
     		} catch (IOException e) {
-    			logger.error("Can't convert patient samplesheet to excel: " + e.getMessage());
-        		throw new DmeSyncMappingException("Can't convert patient samplesheet to excel: " + e.getMessage() , e);
+    			String msg = "Can't convert patient samplesheet to excel: " + e.getMessage();
+     			logger.error(msg, e);
+         		throw new DmeSyncMappingException(msg, e);
         	}
 	    } else if(object.getOriginalFilePath().contains("fastq_files")) {
 	    	//Set source path to the actual file.
@@ -226,7 +227,7 @@ public class SBPathMetadataProcessorImpl extends AbstractPathMetadataProcessor
     String patientCollectionPath = projectCollectionPath + "/Patient_" + patientId;
     HpcBulkMetadataEntry pathEntriesPatient = new HpcBulkMetadataEntry();
     pathEntriesPatient.getPathMetadataEntries().add(createPathEntry(COLLECTION_TYPE_ATTRIBUTE, "Patient"));
-    if(patientKey == null || patientKey.isEmpty()) {
+    if(StringUtils.isBlank(patientKey)) {
     	String msg = "MRN column is empty for file: " + object.getOrginalFileName();
     	logger.info(msg);
 		throw new DmeSyncMappingException(msg);
